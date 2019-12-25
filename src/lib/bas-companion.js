86d7a3c1cf63bbdd0aa7ebb48323b66cfc80d20b
orchid2ev <dev-fronter@gmail.com>
2019-12-25 12:06:07 +0800

    o

--- src/lib/bas-companion.js
+++ src/lib/bas-companion.js
@@ -3,9 +3,14 @@ const debug = require('debug')
 const log = debug('bas-companion:core')
 log.error = debug('bas-companion:core:error')
 
-const browser = require('webextension-polyfill')
+const browser = require('webextension-polyfill'),
+{ createRuntimeInfo } = require('./runtime-info')
 
 module.exports = async function init() {
   /* APIS local cache */
-  var state
+  var state,runtime
+
+  try{
+    runtime = await createRuntimeInfo(browser)
+  }
 }
