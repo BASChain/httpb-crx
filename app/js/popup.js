37bed5a791cd35b6c1ba9cc2cba7815f66958075
orchid2ev <dev-fronter@gmail.com>
2020-01-02 21:34:36 +0800

    add

--- app/js/popup.js
+++ app/js/popup.js
@@ -10,16 +10,16 @@ angular.module('p3OptionsApp',[]).controller('p3Ctrl',['$scope',function($s){
 
   storage.get({"enable_bas":false},function(obj){
     //console.log('mode',JSON.stringify(obj,null,2))
-    $s.mode = obj.enable_bas ? BAS_MODE : DNS_MODE
+    $s.mode = 'v0.0.4'
     $s.$apply()
   })
 
-  $s.chanageMode = function(){
+/*  $s.chanageMode = function(){
     storage.get({"enable_bas":false},function(obj) {
       storage.set({"enable_bas" : !obj["enable_bas"] })
       $s.mode = (!obj["enable_bas"]) ? BAS_MODE : DNS_MODE
       //console.log('>>>',$s.mode)
       $s.$apply();
     })
-  }
+  }*/
 }]);
